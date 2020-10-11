5e7480ef317fc3aec4185ec75d85dadbade6b934
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-10-11 12:22:26 +0530

    Added a 'X' symbol for delete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,23 @@ import '../css/TodoItem.css';
 class TodoItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { status: 'notDone' };
+    this.state = { status: 'notDone', isMouseOnOver: false };
     this.handleChange = this.handleChange.bind(this);
+    this.handleOver = this.handleOver.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
   }
 
   handleChange() {
     const status = { notDone: 'doing', doing: 'done', done: 'notDone' };
     this.setState(state => ({ status: status[state.status] }));
+  }
+
+  handleOver() {
+    this.setState(() => ({ isMouseOnOver: true }));
+  }
+
+  handleLeave() {
+    this.setState(() => ({ isMouseOnOver: false }));
   }
 
   getClass() {
@@ -27,12 +37,19 @@ class TodoItem extends React.Component {
   }
 
   render() {
+    const component = this.state.isMouseOnOver ? <div>X</div> : '';
     return (
-      <div className='item' onClick={this.handleChange}>
+      <div
+        className='item'
+        onClick={this.handleChange}
+        onMouseOver={this.handleOver}
+        onMouseLeave={this.handleLeave}
+      >
         <div className={this.getClass() + ' status'}>&nbsp;</div>
         <div className='itemDescription' style={this.getStyle()}>
           {this.props.item}
         </div>
+        {component}
       </div>
     );
   }
