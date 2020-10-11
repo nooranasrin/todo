5d5413bb2e9a6f38ab30494c2b2c8c02509fcdca
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-10-11 17:58:27 +0530

    Removed one unwanted class

--- src/components/DeleteIcon.js
+++ src/components/DeleteIcon.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const DeleteIcon = props => (
   <div
-    className='deleteIcon'
     onClick={event => {
       event.stopPropagation();
       props.remove(props.id);
