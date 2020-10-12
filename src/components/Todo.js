75b3f9d471de2fa3fb27615f97a3b3bd0bc5c0e3
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-10-12 19:50:15 +0530

    moved Todo into functional component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,63 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoItems from './TodoItems';
 import TodoTitle from './TodoTitle';
 import '../css/Todo.css';
 import InputBox from './InputBox';
 
-class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], id: 0 };
-    this.addItem = this.addItem.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-    this.reset = this.reset.bind(this);
-    this.updateStatus = this.updateStatus.bind(this);
-  }
+const Todo = () => {
+  const [items, setItems] = useState([]);
+  const [id, setId] = useState(0);
 
-  addItem(title) {
-    this.setState(({ items, id }) => ({
-      items: items.concat({ title, status: 'notDone', id: id + 1 }),
-      id: id + 1,
-    }));
-  }
+  const updateState = ({ items, id }) => {
+    setItems(items);
+    setId(id);
+  };
 
-  updateStatus(id) {
-    this.setState(({ items }) => {
-      const status = { notDone: 'doing', doing: 'done', done: 'notDone' };
-      const itemsList = items.map(item => Object.assign({}, item));
-      const item = itemsList.find(item => item.id === id);
-      item.status = status[item.status];
-      return { items: itemsList };
-    });
-  }
+  const updateStatus = id => {
+    const status = { notDone: 'doing', doing: 'done', done: 'notDone' };
+    const itemsList = items.map(item => Object.assign({}, item));
+    const item = itemsList.find(item => item.id === id);
+    item.status = status[item.status];
+    updateState({ items: itemsList, id });
+  };
 
-  removeItem(id) {
-    this.setState(({ items }) => {
-      const itemsList = items.map(item => Object.assign({}, item));
-      const item = itemsList.find(item => item.id === id);
-      const index = itemsList.indexOf(item);
-      itemsList.splice(index, 1);
-      return { items: itemsList };
-    });
-  }
+  const removeItem = id => {
+    const itemsList = items.map(item => Object.assign({}, item));
+    const item = itemsList.find(item => item.id === id);
+    const index = itemsList.indexOf(item);
+    itemsList.splice(index, 1);
+    updateState({ items: itemsList, id });
+  };
 
-  reset() {
-    this.setState(() => ({ items: [], id: 0 }));
-  }
+  const addItem = title => {
+    const itemsList = items.concat({ title, status: 'notDone', id: id + 1 });
+    updateState({ items: itemsList, id: id + 1 });
+  };
 
-  render() {
-    return (
-      <div className='todo'>
-        <TodoTitle reset={this.reset} />
-        <TodoItems
-          items={this.state.items}
-          remove={this.removeItem}
-          updateStatus={this.updateStatus}
-        />{' '}
-        <InputBox onSubmit={this.addItem} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='todo'>
+      <TodoTitle reset={() => updateState({ items: [], id: 0 })} />
+      <TodoItems
+        items={items}
+        remove={removeItem}
+        updateStatus={updateStatus}
+      />{' '}
+      <InputBox onSubmit={addItem} />
+    </div>
+  );
+};
 
 export default Todo;
